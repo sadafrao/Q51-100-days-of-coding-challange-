52ab0eb97a9faf82b4b841ee86b729ed0ebc9bdf
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 20:31:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// take a function that calculate area of rectangle 
+function calculateArea(width, height) {
+    return width * height;
+}
+// refactored into an arrow function 
+// function calculateAreaArrow = (width : number , height : number): number =>
+//  width * height ;
+// example usage of an arrow function 
+console.log(calculateArea);
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Q51  take a function that calculate area of rectangle 
+function calculateArea (width : number , height : number): number{
+ return width * height ;
+}
+
+
+ function calculate arrow(width : number , height : number): number =>  width * height ;
+ //example usage of an arrow function 
+console.log(calculateArea);
+function arrow(width: any, number: any, height: any, number1: any) {
+    throw new Error("Function not implemented.");
+}
+
